20205c7186e59b6a4675a761a05e5e7e88756113
Mike Huynh <hades@Mikes-MBP.attlocal.net>
2021-01-05 14:44:07 -0800

    books data

--- server.js
+++ server.js
@@ -1,27 +1,41 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const {
-    GraphQLSchema,
-    GraphQLObjectType,
-    GraphQLString
-} = require('graphql');
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLList, GraphQLInt, GraphQLNonNull } = require('graphql');
+const { books, arthurs } = require('./data');
 const app = express();
 
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  description: 'This represent a book written by an arthur',
+  fields: () =>({
+    id: { type: GraphQLNonNull(GraphQLInt)},
+    name: { type: GraphQLNonNull(GraphQLString) },
+    authurId: { type: GraphQLNonNull(GraphQLInt)}
+  })
+})
+
+const RootQueryType = new GraphQLObjectType({
+  name: 'Query',
+  description: 'Root Query',
+  fields: () => ({
+    books: {
+      type: new GraphQLList(BookType),
+      description: 'A List of Books',
+      resolve: () => books
+    }
+  })
+})
+
+
 const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-        name: 'HelloWorld',
-        fields: () =>({
-            message: { 
-                type: GraphQLString,
-                resolve: () => 'Hello World'
-            }
-        })
-    })
-});
+  query: RootQueryType
+})
 
-app.use('/graphql', graphqlHTTP({
-    graphiql:true,
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    graphiql: true,
     schema: schema
-}))
-app.listen(5000., ()=>console.log('Server Running'));
-
+  })
+);
+app.listen(5000, () => console.log('Server Running'));
